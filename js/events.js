f0068a91c8cc82529668ec692a5cb9f416f9bf2b
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-12 20:38:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -29,7 +29,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $("form input:submit").on('keydown', function() {
+  $("input:submit").on('submit', function() {
     alert("Your form is going to be submitted now.");
   });
 }
